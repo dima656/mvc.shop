0ebbe9f512ef80fac1adc4ed5799cbbbb26879d7
Dima <anglosax@mail.ru>
2017-01-14 23:41:33 +0200

    some updates

--- config/routes.php
+++ config/routes.php
@@ -2,6 +2,10 @@
 
 
 return[
+    'product/([0-9]+)'=>'product/view/$1',
+    'catalog'=>'catalog/index',
+    'category/([0-9]+)'=>'catalog/category/$1',
+    ''=>'site/index',
     'news/([a-z]+)/([0-9]+)'=>'news/view/$1/$2',
     'news/([0-9]+)'=>'news/view/$1',
 
